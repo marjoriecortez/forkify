e54fa8ee51061d13d4f89bf0408012027990f000
Marjorie <marjorie.crtz@yahoo.com>
2024-03-08 15:32:33 +0800

    Implement search results

--- src/js/model.js
+++ src/js/model.js
@@ -4,6 +4,10 @@ import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
@@ -30,6 +34,19 @@ export const loadRecipe = async function (id) {
 
 export const loadSearchResults = async function (query) {
   try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
   } catch (err) {
     console.error(`${err} 💥💥💥💥`);
     throw err;
